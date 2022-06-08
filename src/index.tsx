818902d5d8507b037c543fce62184854dfb472ee
mishashmidt <mishashmidt0@gmail.com>
2022-06-08 23:05:08 +0300

    feat: add thunk getTodo

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from "./pages/App";
 import {store} from "./store/redux";
 import {Provider} from "react-redux";
 
 
-ReactDOM.render(
-    <Provider store={store}>
-        <App/>
-    </Provider>, document.getElementById('root'));
-
-
-
+const container = document.getElementById('root');
+const root = createRoot(container as HTMLElement);
+root.render(<Provider store={store}><App/> </Provider>);
