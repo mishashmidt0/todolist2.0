485a7e43539114398403df8e10a3c541b1d1a098
mishashmidt <mishashmidt0@gmail.com>
2022-06-17 22:18:23 +0300

    fix: change header

--- src/components/Todolist/Header.tsx
+++ src/components/Todolist/Header.tsx
@@ -1,11 +1,10 @@
 import React, {useCallback} from "react";
-import {AppBar, Button, IconButton, Toolbar, Typography} from "@material-ui/core";
-import {Menu} from "@material-ui/icons";
+import {AppBar, Button, Toolbar} from "@material-ui/core";
 import {useDispatch, useSelector} from "react-redux";
 import {storeType} from "../../store/redux";
 import {logoutTC} from "../login/auth-reducer";
 import {Dispatch} from "redux";
-
+import LogoutIcon from '@mui/icons-material/Logout';
 
 export const Header = React.memo(() => {
     const isLoggedIn = useSelector<storeType, boolean>(state => state.auth.isLoggedIn)
@@ -13,16 +12,11 @@ export const Header = React.memo(() => {
     const logoutHandler = useCallback(() => {
         dispatch(logoutTC())
     }, [])
+
     return (
         <AppBar position="static">
             <Toolbar>
-                <IconButton edge="start" color="inherit" aria-label="menu">
-                    <Menu/>
-                </IconButton>
-                <Typography variant="h6">
-                    News
-                </Typography>
-                {isLoggedIn && <Button color="inherit" onClick={logoutHandler}>Log out</Button>}
+                {isLoggedIn && <Button color="inherit" onClick={logoutHandler} endIcon={<LogoutIcon/>}>Log out</Button>}
             </Toolbar>
 
         </AppBar>
